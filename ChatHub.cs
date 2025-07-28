238761b40183c1d4dacbb869c93753de096bb39f
cocyaaa <coc504yaaa@gmail.com>
2025-07-29 00:00:20 +0800

    Initial commit

---
+++ ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace APP2signalr
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageToAll( string message)
+        {
+            Console.WriteLine(message);
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
+
